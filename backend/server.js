9a1c4243abe2a5e70bf919e9c87b8308a61f25b3
Eman Oudh <emanoudh2021@gmail.com>
2026-07-05 03:25:59 +0300

    Use Render port

--- backend/server.js
+++ backend/server.js
@@ -164,7 +164,7 @@ console.log("📧 Email sent successfully!");
     }
   }
 });
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server running on http://localhost:" + PORT);
